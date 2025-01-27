3898bf8b26e0c09af66927c9db629c4e4fd2a580
nada912 <nada.nadire@efrei.net>
2025-01-27 14:15:03 +0100

    mock test supabase

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -4,15 +4,22 @@ from mlflow.tracking import MlflowClient
 from dotenv import load_dotenv
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from backend.app import app
+from supabase import create_client
 
 # Load environment variables
 load_dotenv()
 
 MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL", "")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
+
+if SUPABASE_URL and SUPABASE_KEY:
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+else:
+    supabase = None  # No connection in test mode
+
 
 def test_load_model_from_mlflow():
     """
@@ -36,8 +43,14 @@ def test_load_model_from_mlflow():
     assert model is not None, "The model should not be None"
 
 
-def test_predict_route():
-    """Test the predict route with valid input."""
+from unittest.mock import patch, Mock
+
+@patch("backend.connect_supabase.create_client")
+def test_predict_route(mock_create_client):
+    mock_client = Mock()
+    mock_create_client.return_value = mock_client
+    mock_client.table.return_value.insert.return_value.execute.return_value = {"data": "mock_response"}
+
     client = app.test_client()
     response = client.post('/predict', json={
         "area": 1200, "bedrooms": 2, "bathrooms": 1, "stories": 1,
@@ -45,8 +58,10 @@ def test_predict_route():
         "hotwaterheating": "no", "airconditioning": "no",
         "parking": 1, "prefarea": "no", "furnishingstatus": "semi-furnished"
     })
+
     assert response.status_code == 200
     assert "predicted_price" in response.get_json()
+
 
 
 def test_predict_route_invalid_input():
