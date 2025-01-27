63dd74df88eff6d7debe3a59b9169cab887227e8
nada912 <nada.nadire@efrei.net>
2025-01-27 04:36:15 +0100

    update unit test

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,12 +1,36 @@
 import pytest
-from app import app, preload_model
+import mlflow.sklearn
+from mlflow.tracking import MlflowClient
+from dotenv import load_dotenv
+import os
+from backend.app import app
 
-def test_preload_model():
-    """Test that the model is preloaded successfully."""
+# Load environment variables
+load_dotenv()
+
+MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
+
+def test_load_model_from_mlflow():
+    """
+    Test loading the model from MLflow.
+    """
+    # set the MLflow tracking URI from environment variables
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+
+    model_name = "PolynomialRegressionModel"
+    client = MlflowClient()
+
+    # Get the latest version of the model in the Production stage
+    latest_version = client.get_latest_versions(model_name, stages=["Production"])[0].version
+    model_uri = f"models:/{model_name}/{latest_version}"
+    
     try:
-        preload_model()
+        model = mlflow.sklearn.load_model(model_uri)
     except Exception as e:
-        pytest.fail(f"Model preload failed: {str(e)}")
+        pytest.fail(f"The model could not be loaded from MLflow. Error : {e}")
+    
+    assert model is not None, "The model should not be None"
+
 
 def test_predict_route():
     """Test the predict route with valid input."""
@@ -19,6 +43,7 @@ def test_predict_route():
     })
     assert response.status_code == 200
     assert "predicted_price" in response.get_json()
+
 
 def test_predict_route_invalid_input():
     """Test the predict route with invalid input."""
